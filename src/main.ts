20a2ca6a41cc43f7408bf5a4db8c9a7e11a0463f
carraes <carraeshb@gmail.com>
2022-12-26 16:55:35 -0300

    feat: class User

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,9 @@
-console.log('Test')
+import { User } from './models/User'
+
+const user = new User({ name: 'myname', age: 20 })
+
+user.on('change', () => console.log('Change #1'))
+user.on('change', () => console.log('Change #2'))
+user.on('click', () => console.log('Change #3'))
+
+user.trigger('click')
