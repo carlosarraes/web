04cf96e8d49ed6e9d1b90c174381466c25719fb6
carraes <carraeshb@gmail.com>
2022-12-26 23:59:06 -0300

    feat: Eventing, Sync, User classes

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,3 @@
 import { User } from './models/User'
 
-const user = new User({ name: 'myname', age: 20 })
-
-user.on('change', () => console.log('Change #1'))
-user.on('change', () => console.log('Change #2'))
-user.on('click', () => console.log('Change #3'))
-
-user.trigger('click')
+const user = new User({ name: 'new record', age: 0 })
