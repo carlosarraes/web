bf8dc605fdedfbb47f212fdbbb1f168a41894945
carraes <carraeshb@gmail.com>
2022-12-28 11:31:54 -0300

    feat: more components

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
+import { User } from './models/User'
 import { UserForm } from './views/UserForm'
 
-const userForm = new UserForm(document.getElementById('root') as Element)
+const user = User.buildUser({ name: 'Lulu', age: 20 })
+
+const userForm = new UserForm(document.getElementById('root') as Element, user)
 
 userForm.render()
